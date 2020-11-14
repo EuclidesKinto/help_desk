ed9c497315a9317885c1b427e992b3894d731f30
Euclides <euclideskinto@gmail.com>
2020-11-14 10:24:48 -0300

    Autenticando Usuário

--- valida_login.php
+++ valida_login.php
@@ -1,1 +1,19 @@
 <?php
+
+$user_autenticado = false;
+$usuarios_app = [
+  ['email' => 'admin@hotmail.com', 'password' => '123456'],
+  ['email' => 'kinto@hotmail.com', 'password' => '987654'],
+];
+
+foreach ($usuarios_app as $user){
+    if ($user['email'] == $_POST['email'] && $user['password'] == $_POST['password']){
+        $user_autenticado = true;
+    }
+}
+
+if($user_autenticado){
+    echo 'Usuário autenticado!';
+}else{
+   header('Location: index.php?login=error');
+}
